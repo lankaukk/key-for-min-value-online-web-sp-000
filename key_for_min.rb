a2f297a24bb082cbf79ff008f6c54f2e62708a2c
McKayla Lankau <lankaukk@yahoo.com>
2020-08-15 03:27:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -15,7 +15,7 @@ def key_for_min_value(name_hash)
       if i[1] < d_value
         d_value = i[1]
         d_key = i[0]
-      elsif name_a.index == 0 
+      elsif name_a.index == 1 
         nil
       end
 
