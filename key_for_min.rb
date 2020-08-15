8a9f5d8d3bb0bac24fd0a58ed6866001aa0bd0f4
McKayla Lankau <lankaukk@yahoo.com>
2020-08-15 03:30:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,17 +5,14 @@ def key_for_min_value(name_hash)
   # Convert hash to array
   name_a = name_hash.to_a
 
-  # Default key value
   d_value= 10000
   d_key= nil
 
-  # Iterate new array 
   name_a.each do |i|
-      # If current value is lower than default, change value&key 
       if i[1] < d_value
         d_value = i[1]
         d_key = i[0]
-      elsif name_a.index == 1 
+      elsif 
         nil
       end
 
