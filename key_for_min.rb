3f8d57c1e746542ee744ba0439e1d22a17e401e7
McKayla Lankau <lankaukk@yahoo.com>
2020-08-15 03:20:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.each do |key, value|
+  name_hash.each do |:key, value|
     if value < value 
-      puts "#{key}"
-  
-      
+      puts "#{:key}"
     end
   end
 end
