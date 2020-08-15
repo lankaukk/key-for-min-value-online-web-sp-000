154b5d2222ab93dbd09ee6ced105e35082df3bb8
McKayla Lankau <lankaukk@yahoo.com>
2020-08-15 03:23:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   name_hash.each do |name, value|
-    if value[0] > value[1] > value[2]
+    if value[0] < value[1] < value[2]
       puts "#{name}"
     end
   end
